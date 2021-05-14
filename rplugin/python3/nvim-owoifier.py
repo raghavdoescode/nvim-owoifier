8584acafb1b4549f0be45280079fad461ed5f89e
nerdthatnoonelikes <kenrag24@gmail.com>
2021-05-14 09:00:12 -0500

    cursed plugin works

---
+++ rplugin/python3/nvim-owoifier.py
@@ -0,0 +1,30 @@
+import neovim
+
+@neovim.plugin
+class Plugin(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+    @neovim.command("OWOify")
+    def owoify(self):
+        file_path = self.vim.current.buffer.name
+        with open(file_path, 'r+') as file:
+            data = file.readlines()
+            finaltext = ''
+
+            for i in range(0, len(data)):
+                for char in data[i]:
+                    if char == 'R':
+                        finaltext = finaltext + 'W'
+                    elif char == 'r':
+                        finaltext = finaltext + 'w'
+                    elif char == 'L':
+                        finaltext = finaltext + 'W'
+                    elif char == 'l':
+                        finaltext = finaltext + 'w'
+                    else:
+                        finaltext = finaltext + char
+            file.seek(0) 
+            file.truncate() 
+            file.write(finaltext)
+            file.close()
